b9a5e92c7e1b0d7542314a00dd60570eac0263e1
EricEisaman <eeisaman@auburnschl.edu>
2020-03-25 18:19:41 -0400

    test hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import HomeUI from './UIs/home-ui';
 
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(()=>{
+    fetch('/time').then(res=>res.json()).then(data=>{
+      setCurrentTime(data.time); 
+    })
+  },[]);
   return (
     <div className="App">
       <HomeUI />
+      <p>The current time is {currentTime}.</p>
     </div>
   );
 }
