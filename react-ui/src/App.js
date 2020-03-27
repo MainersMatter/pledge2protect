aa71a6064e6c2d525968ccc828583ec20f7cfda0
LFMB <lfmblinux@LFMBComp.localdomain>
2020-03-27 14:43:47 -0400

    got a basic landing page looking good - time to link form to MySQL database

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-import HomeUI from './UIs/home-ui';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import HomeUIWrapper from './components/home-ui-wrapper';
+
+import './base.css';
 
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res=>res.json()).then(data=>{
+      setCurrentTime(data.time); 
+    })
+  },[]);
+
+  library.add(fab);
+
   return (
-    <div className="App">
-      <HomeUI />
+    <div className="app-wrapper">
+      <HomeUIWrapper />      
     </div>
   );
 }
