1a709875164daad32b64bf465bba1bc2ed48b3c1
Ian <ian@nchls.com>
2020-03-26 22:21:56 -0400

    Fix indentation on .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    'airbnb',
-  ],
-  globals: {
-    test: 'readonly',
-    expect: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  settings: {
-    react: {
-      version: '16.13',
+    extends: [
+        'airbnb',
+    ],
+    globals: {
+        test: 'readonly',
+        expect: 'readonly',
     },
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    'indent': ['error', 4],
-    'react/jsx-indent': [2, 4],
-    'no-console': 0,
-    'no-debugger': 0,
-    'no-plusplus': 0,
-    'react/jsx-filename-extension': 0,
-  },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    settings: {
+        react: {
+            version: '16.13',
+        },
+    },
+    plugins: [
+        'react'
+    ],
+    rules: {
+        'indent': ['error', 4],
+        'react/jsx-indent': [2, 4],
+        'no-console': 0,
+        'no-debugger': 0,
+        'no-plusplus': 0,
+        'react/jsx-filename-extension': 0,
+    },
 };
