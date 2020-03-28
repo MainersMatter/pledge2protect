e065e4d8c7e94d66566c0cdb474cc0b964203453
Erin Doyle <erin.doyle@bhnetwork.com>
2020-03-28 19:59:07 -0400

    Added the subscribing of the pledged user to our email list

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const bodyParser = require('body-parser');
 
-const { getCountUserPledges, saveUser } = require('./user');
+const { getCountUserPledges, savePledge, addEmailSubscriber } = require('./user');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
@@ -13,8 +14,8 @@ function runServer() {
 
     // Priority serve any static files.
     app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-    app.use(express.json()) // for parsing application/json
-    app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+    app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
     app.get('/pledge/count', (req, res) => {
         res.set('Content-Type', 'application/json');
@@ -26,17 +27,33 @@ function runServer() {
         });
     });
 
-    app.post('/user', (req, res) => {
+    app.post('/pledge', async (req, res) => {
         const user = req.body;
+        user.hasPledged = true;
 
         // validate the user data
         if(!user || !user.emailAddress || !user.firstName || !user.lastName){
             res.status(400).send({ error:true, message: 'Please provide user emailAddress, firstName and lastName' });
         }
-        else{
-            saveUser(user, function(err, user) {
-                if (err)
+
+        try {
+            // subscribe the pledged user to our email list
+            const subscribedResult = await addEmailSubscriber(user);
+
+            // add the subscribed email id from the MailChimp list to the user
+            user.subscribedEmailId = subscribedResult.id;
+        } catch (error) {
+            res.status(500)
+                .send({ error: true, message: error.message });
+            return;
+        }
+
+            // save the user in the database
+            savePledge(user, async function(err, user) {
+                if (err) {
                     res.send(err);
+                }
+
                 res.send('success');
             });
         }
