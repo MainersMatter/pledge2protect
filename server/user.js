9aefe903b0f2adbf1922f45e3c722bdc046bcc0c
Erin Doyle <erin.doyle@bhnetwork.com>
2020-03-28 19:59:07 -0400

    Added the subscribing of the pledged user to our email list

--- server/user.js
+++ server/user.js
@@ -1,10 +1,52 @@
 const mysql = require('mysql');
+const Mailchimp = require('mailchimp-api-v3');
 
 const connection = require('./db.js');
 
-exports.saveUser = (user, result) => {
+
+const mailchimp = process.env.MAILCHIMP_API_KEY ? new Mailchimp(process.env.MAILCHIMP_API_KEY) : {};
+
+exports.addEmailSubscriber = async (user) => {
+    if (!mailchimp || !process.env.MAILCHIMP_LIST_ID) {
+        const errorMessage = 'Unable to add email subscriber - missing Email Service configuration';
+        throw Error(errorMessage);
+    }
+
+    try {
+        const result = await mailchimp.post(`lists/${process.env.MAILCHIMP_LIST_ID}/members`, {
+            email_address: user.emailAddress.toLowerCase(),
+            status: 'pending',
+            merge_fields: {
+                'MMERGE6': user.firstName,
+                'MMERGE1': user.lastName,
+                'ADDRESS[addr1]': user.addressLine1,
+                'ADDRESS[addr2]': user.addressLine2,
+                'ADDRESS[city]': user.city,
+                'ADDRESS[state]': user.state,
+                'ADDRESS[zip]': user.zipCode,
+                'ADDRESS[country]': 'USA'
+            }
+        });
+
+        if (result.errors) {
+            console.error(`result.errors: ${result.errors}`);
+            throw Error(result.errors);
+        }
+
+        return result;
+    } catch (err) {
+        let errorMessage = '\n';
+        err.errors.forEach(fieldError => {
+            errorMessage += `    ${fieldError.field}: ${fieldError.message}\n`;
+        });
+        console.error(`Error adding new email subscriber: ${errorMessage}`);
+        throw Error('Error adding new email subscriber');
+    }
+};
+
+exports.savePledge = (user, callback) => {
     const userRecord = {
-        'email_address': user.emailAddress,
+        'email_address': user.emailAddress.toLowerCase(),
         'first_name': user.firstName,
         'last_name': user.lastName,
         'address_line_1': user.addressLine1,
@@ -12,24 +54,25 @@ exports.saveUser = (user, result) => {
         'city': user.city,
         'state': user.state,
         'zip_code': user.zipCode,
-        'has_pledged': user.hasPledged
+        'has_pledged': user.hasPledged,
+        'subscribed_email_id': user.subscribedEmailId
     };
 
     const sql = 'INSERT INTO user set ?';
     connection.query(sql, userRecord, function (error, results) {
         if (error) {
-            result(error, null);
+            callback(error, null);
         }
         else{
-            result(null, results.affectedRows);
+            callback(null, results.affectedRows);
         }
     });
 };
 
-exports.getCountUserPledges = (result) => {
+exports.getCountUserPledges = (successCallback) => {
     const sql = 'SELECT count(email_address) pledges FROM user WHERE has_pledged = true';
     connection.query(sql, function (error, results, fields) {
         if (error) throw error;
-        result(results[0]);
+        successCallback(results[0]);
     });
 };
