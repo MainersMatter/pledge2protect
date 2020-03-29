b48f540173569f7b1ad90fb4efd25c73fd6ae90e
Caroline Whitman <cwhitman@cashstar.com>
2020-03-29 11:28:15 -0400

    Updated grid styling to add email to field

--- react-ui/src/components/Pledge/PledgeForm.js
+++ react-ui/src/components/Pledge/PledgeForm.js
@@ -8,39 +8,43 @@ const PledgeForm = () => {
     const { register, handleSubmit, errors } = useForm();
     const onSubmit = data => {
         console.log(data);
-        // fetch('/user', {
+        // TODO: Make sure that pledge form data is correctly posting to endpoint, add error handling
+        // fetch('/pledge', {
         //     method: 'POST',
         //     body: data
-        // });
+        // });s
     };
     return (
         <div className="pledge-form-container">
           <form className="pledge-form" onSubmit={handleSubmit(onSubmit)}>
               <div className='col-1_row-1'>
-                  <label>First Name:</label>
+                  <label>Email*:</label>
+                  <input name='email' ref={register({ required: true })}/>
+                  { errors.email && <p className='error'>This field is required</p> }
+              </div>
+              <div className='col-1_row-2'>
+                  <label>First Name*:</label>
                   <input name='firstName' ref={register({ required: true })}/>
                   { errors.firstName && <p className='error'>This field is required</p> }
               </div>
-              <div className='col-2_row-1'>
-                  <label>Last Name:</label>
+              <div className='col-2_row-2'>
+                  <label>Last Name*:</label>
                   <input name='lastName' ref={register({ required: true })}/>
                   { errors.lastName && <p className='error'>This field is required</p> }
               </div>
-              <div className='col-1_row-2'>
+              <div className='col-1_row-3'>
                   <label>Address Line 1:</label>
                   <input name='addressLineOne' ref={register({ required: true })}/>
-                  { errors.addressLineOne && <p className='error'>This field is required</p> }
               </div>
-              <div className='col-2_row-2'>
+              <div className='col-2_row-3'>
                   <label>Address Line 2:</label>
                   <input name='addressLineTwo'/>
               </div>
-              <div className='col-1_row-3'>
+              <div className='col-1_row-4'>
                   <label>City:</label>
                   <input name='city' ref={register({ required: true })}/>
-                  { errors.city && <p className='error'>This field is required</p> }
               </div>
-              <div className='col-2_row-3'>
+              <div className='col-2_row-4'>
                   <label>State:</label>
                   <select name='state-input' ref={register({ required: true })}>
                       { stateMapping.map((state, index) => {
@@ -48,14 +52,13 @@ const PledgeForm = () => {
                         })
                       }
                   </select>
-                  { errors.state && <p className='error'>This field is required</p> }
-                  </div>
-              <div className='col-1_row-4'>
-                  <label>Zip Code:</label>
-                  <input name='zipCode' ref={register({ required: true, maxLength: 5 })}/>
-                  { errors.zipCode && <p className='error'>This field is required</p> }
               </div>
               <div className='col-1_row-5'>
+                  <label>Zip Code:</label>
+                  <input name='zipCode' ref={register({ required: true, maxLength: 5 })}/>
+              </div>
+              <div className='col-1_row-6'>
+                  <label>* Indicates this field is required.</label>
                 <input className='btn' type='submit' value='Take The Pledge'/>
               </div>
           </form>
