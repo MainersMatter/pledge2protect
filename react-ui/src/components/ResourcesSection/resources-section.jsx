5e23a7c63c2cc88800132ec28451934e59985ecd
Ian <ian@nchls.com>
2020-03-29 19:02:36 -0400

    Bottom resources section and social networks widget

---
+++ react-ui/src/components/ResourcesSection/resources-section.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './resources-section.scss';
+import SocialIcons from '../SocialIcons/social-icons';
+import chevronRight from '../../assets/chevron-right.svg';
+
+
+const ResourcesSection = () => (
+    <div className="resources-section">
+        <a className="resource-box connections" href="/">
+            <span>
+                COVID-19: Connections &amp; Resources
+            </span>
+            <div className="chevron">
+                <img src={chevronRight} aria-hidden="true" alt="" />
+            </div>
+        </a>
+        <div className="resource-box social">
+            <p>Follow us on social media:</p>
+            <SocialIcons mode="follow" />
+        </div>
+    </div>
+);
+
+export default ResourcesSection;
---
+++ react-ui/src/components/SocialIcons/social-icons.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './social-icons.scss';
+
+
+const siteUrl = 'https://www.pledgetoprotectme.org/';
+const socialShareBlurb = 'Check this out';
+const encodedSiteUrl = encodeURIComponent(siteUrl);
+const encodedShareBlurb = encodeURIComponent(socialShareBlurb);
+
+const socialNetworkUrls = {
+    facebook: {
+        follow: 'https://www.facebook.com/',
+        share: `https://www.facebook.com/sharer/sharer.php?u=${encodedSiteUrl}`,
+    },
+    linkedin: {
+        follow: 'https://www.linkedin.com/',
+        share: `https://www.linkedin.com/shareArticle?mini=true&url=${encodedSiteUrl}&summary=${encodedShareBlurb}`,
+    },
+    twitter: {
+        follow: 'https://twitter.com/',
+        share: `https://twitter.com/intent/tweet?text=${encodedShareBlurb}&url=${encodedSiteUrl}`,
+    },
+    instagram: {
+        follow: 'https://www.instagram.com/',
+        // Instagram does not support share links
+    },
+};
+
+const SocialIcons = ({ mode }) => (
+    <div className="social-icons">
+        <a href={socialNetworkUrls.facebook[mode]} className="facebook" title="Facebook">
+            <span className="sr-only">
+                Facebook
+            </span>
+        </a>
+        <a href={socialNetworkUrls.linkedin[mode]} className="linkedin" title="LinkedIn">
+            <span className="sr-only">
+                LinkedIn
+            </span>
+        </a>
+        <a href={socialNetworkUrls.twitter[mode]} className="twitter" title="Twitter">
+            <span className="sr-only">
+                Twitter
+            </span>
+        </a>
+        { mode === 'follow' && (
+            <a href={socialNetworkUrls.instagram[mode]} className="instagram" title="Instagram">
+                <span className="sr-only">
+                    Instagram
+                </span>
+            </a>
+        ) }
+    </div>
+);
+
+SocialIcons.propTypes = {
+    mode: PropTypes.oneOf(['follow', 'share']).isRequired,
+};
+
+export default SocialIcons;
