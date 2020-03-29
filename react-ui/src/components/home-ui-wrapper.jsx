b9663aa8afdc15cc55fe23424cb48fd1d5def14b
Caroline Whitman <cwhitman@cashstar.com>
2020-03-28 22:38:18 -0400

    added grid styling for form

--- react-ui/src/components/home-ui-wrapper.jsx
+++ react-ui/src/components/home-ui-wrapper.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import MainContentBlock from './main-content-block';
 
 import './styles.home-ui-wrapper.scss';
+import PledgeForm from "./Pledge/PledgeForm";
 
 
 function HomeUIWrapper() {
     return (
         <div className="home-ui-wrapper">
             <MainContentBlock />
+            <PledgeForm/>
         </div>
     );
 }
