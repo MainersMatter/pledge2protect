d1ec499afd71a06aaacdc52a7a5a6f7ca5082019
Caroline Whitman <cwhitman@cashstar.com>
2020-03-29 13:59:16 -0400

    added why-pledge section

---
+++ react-ui/src/components/why-pledge.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.why-pledge.css';
+
+const WhyPledge = () => (
+    <div className='why-pledge-content'>
+        <h2 className='why-pledge-header'>Why Pledge?</h2>
+        <p className='why-pledge-body'><strong>COVID-19</strong> is spreading fast in Maine. Anyone can get it, anyone
+            can spread it and it can be deadly. <strong>Physically distancing</strong> yourself
+        from other is the strongest way to reduce the spread of the virus and
+        protect yourself, your family and your neighbors. <strong>Together</strong>, we can stop
+        COVID-19 in its tracks. <strong>You can make a difference.</strong></p>
+        <h3 className='why-pledge-header'>Add your name to Pledge to Protect Maine ></h3>
+    </div>
+);
+
+export default WhyPledge;
