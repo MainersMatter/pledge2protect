cb7e174a7621f2fcd2531a74e815cc7103928dc5
Caroline Whitman <cwhitman@cashstar.com>
2020-03-30 20:01:12 -0400

    changes contact us page to be a mailto link in the footer

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,7 +6,6 @@ import HomeUIWrapper from './components/home-ui-wrapper';
 
 // page imports
 import PrivacyPage from './pages/privacy.page.jsx';
-import ContactUsPage from './pages/contact-us.page.jsx';
 import NewsPage from './pages/news.page.jsx';
 import ResourcesPage from './pages/resources.page.jsx';
 import WhyTakePledgePage from './pages/why-take-pledge.page.jsx';
@@ -35,7 +34,6 @@ function App() {
     		<Route  path='/why-take-pledge' component={WhyTakePledgePage} />
     		<Route  path='/resources' component={ResourcesPage} />
     		<Route  path='/news' component={NewsPage} />
-    		<Route  path='/contact-us' component={ContactUsPage} />
     		<Route  path='/privacy' component={PrivacyPage} />
 	    </Switch>
       <Footer />
--- react-ui/src/components/footer/Support.js
+++ react-ui/src/components/footer/Support.js
@@ -5,7 +5,7 @@ const Support = () => {
         <div className='support'>
             <h4 className='about-us-header'>Support</h4>
             <p className='about-us-body'>
-                <a className='about-us-links' href="/contact-us">Contact Us</a>
+                <a className='about-us-links' href="mailto:dave@pledgetoprotectme.org">Contact Us</a>
             </p>
             <a className='about-us-links' href="/news">News</a>
         </div>
