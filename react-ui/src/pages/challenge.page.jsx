515b7289d7b65c0cd2097c4fcf3568c26471640c
garam11358 <62483993+garam11358@users.noreply.github.com>
2020-04-24 12:49:32 -0400

    Update challenge.page.jsx

--- react-ui/src/pages/challenge.page.jsx
+++ react-ui/src/pages/challenge.page.jsx
@@ -8,6 +8,7 @@ import LikeAndFollowChallenge from '../components/Challenges/like-and-follow-cha
 import ShowOffChallenge from '../components/Challenges/show-off-challenge';
 import twitterIcon from '../assets/twitter-icon.svg';
 import SocialIcons from '../components/SocialIcons/social-icons';
+import self-monitor from '../components/Challenges/self-monitor
 
 import './challenge-page.scss';
 
